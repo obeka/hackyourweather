a3624b725674ffcecf3712694460aa12d37041c3
Ömer <omerbkk06@gmail.com>
2020-07-25 12:15:28 +0200

    week3-homework-omer-kilic

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import Alert from './Alert';
 import './SearchBox.css';
+import Alert from './Alert';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function SearchBox({setForecastDatas, forecastDatas}) {
+export default function SearchBox({setForecastData, forecastData}) {
     const [cityName, setCityName] = useState('');
     const [isLoading, setLoading] = useState(false); //loading effect
     const [hasError, setError] = useState({status : false});
@@ -11,34 +12,34 @@ export default function SearchBox({setForecastDatas, forecastDatas}) {
 
     const handleSubmit = e => {
         e.preventDefault();
-        const duplicateSearch = forecastDatas.some(singleData => singleData.name.toLowerCase() === cityName.toLowerCase()); // prevent user enter the same city entered before
+        const duplicateSearch = forecastData.some(singleCity => singleCity.response.name.toLowerCase() === cityName.toLowerCase()); // prevent user enter the same city entered before
         if(!duplicateSearch) {
             getCity(cityName);
         } else {
-            setError({status: true, text: { message :'Duplicate search, please check your terms'}, type: 'duplicate'}); //Client side - Duplicate entry error
+                setError({status: true, text: { message :'Duplicate search, please check your terms'}, type: 'duplicate'}); //Client side - Duplicate entry error
         }
     }
 
     useEffect(() => {
-       
         const timer = setTimeout(() => {
-            setError({status : false})
+            setError({status: false})
         }, 3000);
-        return () => clearTimeout(timer);
-
-    },[hasError.status] )
+        return () => clearTimeout(timer)
+    }, [hasError.status])
 
     const getCity = async (cityName) => {
         setLoading(true);
         setError({status: false})
         try {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`);
-            const forecast = await response.json();
-            if(forecast.cod === 200) {
-                setForecastDatas([...forecastDatas, forecast]);
+            const rawResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`);
+            const response = await rawResponse.json();
+            if(response.cod === 200) {
+                const forecast = { response, id: uuidv4()};
+                setForecastData([...forecastData, forecast]);
+                setCityName('')
             } else {
                 //Client side errors - Improper city name
-                setError({status: true, text: forecast, type: 'client'})
+                setError({status: true, text: response, type: 'client'})
             }
             setLoading(false);
         } catch (error) {
@@ -51,7 +52,7 @@ export default function SearchBox({setForecastDatas, forecastDatas}) {
         <>
             <form onSubmit={handleSubmit}>
                 <input type="text" value={cityName} onChange={handleCityName} placeholder='Enter a city name...'
-                    required />
+                    required className={hasError.type === 'client' ? 'input-danger' : hasError.type ==='duplicate' ? 'input-duplicate' : ''}/>
                 <button type='submit'>Get Weather</button>
                 {hasError.status && <Alert hasError={hasError} />}
                 {/* Loading effect from outer source */}
